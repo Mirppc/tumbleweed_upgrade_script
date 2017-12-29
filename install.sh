3a23206415a21b60a24fcc7d8cde5299d57fd151
Mir_ppc <maninredd@yahoo.com>
2017-12-29 13:35:10 -0800

    adding /
    added / at the end of some url's because of errors encountered.

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #add repositories
-zypper ar -f -c http://download.opensuse.org/tumbleweed/repo/oss repo-oss
-zypper ar -f -c http://download.opensuse.org/tumbleweed/repo/non-oss repo-non-oss
-zypper ar -f -c http://download.opensuse.org/tumbleweed/repo/debug repo-debug
+zypper ar -f -c http://download.opensuse.org/tumbleweed/repo/oss/ repo-oss
+zypper ar -f -c http://download.opensuse.org/tumbleweed/repo/non-oss/ repo-non-oss
+zypper ar -f -c http://download.opensuse.org/tumbleweed/repo/debug/repo-debug
 zypper ar -f -c http://download.opensuse.org/update/tumbleweed/ repo-update
 zypper ar -f -d -c http://download.opensuse.org/tumbleweed/repo/src-oss repo-src-oss
 zypper ar -f -d -c http://download.opensuse.org/tumbleweed/repo/src-non-oss repo-src-non-oss
