1da1ffdd3dc5fdda5ed84b41035c48834271d297
Mir_ppc <maninredd@yahoo.com>
2017-12-28 20:16:19 -0800

    remove -n
    -n causes issues with 10.3

--- install.sh
+++ install.sh
@@ -6,11 +6,11 @@ zypper ar -f -c http://download.opensuse.org/tumbleweed/repo/debug repo-debug
 zypper ar -f -c http://download.opensuse.org/update/tumbleweed/ repo-update
 zypper ar -f -d -c http://download.opensuse.org/tumbleweed/repo/src-oss repo-src-oss
 zypper ar -f -d -c http://download.opensuse.org/tumbleweed/repo/src-non-oss repo-src-non-oss
-zypper ar -f -n -c packman http://ftp.gwdg.de/pub/linux/misc/packman/suse/openSUSE_Tumbleweed/ packman
-zypper ar -f -n -c http://download.opensuse.org/repositories/games/openSUSE_Tumbleweed/ games
-zypper ar -f -n -c https://download.opensuse.org/repositories/X11:/XOrg/openSUSE_Tumbleweed/ xorg_x11
-zypper ar -f -n -c https://download.opensuse.org/repositories/hardware/openSUSE_Tumbleweed/ hardware
-zypper ar -f -n -c https://download.opensuse.org/repositories/Emulators/openSUSE_Tumbleweed/ emulators
+zypper ar -f -c packman http://ftp.gwdg.de/pub/linux/misc/packman/suse/openSUSE_Tumbleweed/ packman
+zypper ar -f -c http://download.opensuse.org/repositories/games/openSUSE_Tumbleweed/ games
+zypper ar -f -c https://download.opensuse.org/repositories/X11:/XOrg/openSUSE_Tumbleweed/ xorg_x11
+zypper ar -f -c https://download.opensuse.org/repositories/hardware/openSUSE_Tumbleweed/ hardware
+zypper ar -f -c https://download.opensuse.org/repositories/Emulators/openSUSE_Tumbleweed/ emulators
 #refresh repositories getting GPG keys and package lists
 zypper ref -non-interactive
 #upgrade zypper as this is importiant especialy on pre Leap based systems.  
