17051873320353972554c9db4196697392a314bf
Mir_ppc <maninredd@yahoo.com>
2017-12-29 20:15:29 -0800

    update install.sh fixing packman
    The Packman line has an error making it impossible to add the repo.

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ zypper ar -f -c http://download.opensuse.org/tumbleweed/repo/debug/repo-debug
 zypper ar -f -c http://download.opensuse.org/update/tumbleweed/ repo-update
 zypper ar -f -d -c http://download.opensuse.org/tumbleweed/repo/src-oss repo-src-oss
 zypper ar -f -d -c http://download.opensuse.org/tumbleweed/repo/src-non-oss repo-src-non-oss
-zypper ar -f -c packman http://ftp.gwdg.de/pub/linux/misc/packman/suse/openSUSE_Tumbleweed/ packman
+zypper ar -f -c http://ftp.gwdg.de/pub/linux/misc/packman/suse/openSUSE_Tumbleweed/ packman
 zypper ar -f -c http://download.opensuse.org/repositories/games/openSUSE_Tumbleweed/ games
 zypper ar -f -c https://download.opensuse.org/repositories/X11:/XOrg/openSUSE_Tumbleweed/ xorg_x11
 zypper ar -f -c https://download.opensuse.org/repositories/hardware/openSUSE_Tumbleweed/ hardware
