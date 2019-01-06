8447cb80c71494b7f68a7cd69b581cdc79b4d5f1
Mir_ppc <maninredd@yahoo.com>
2019-01-06 14:30:52 -0800

    Update spellcheck
    added some information, fixed a broken add repo line, spell checked.

--- install.sh
+++ install.sh
@@ -2,10 +2,10 @@
 #add repositories
 zypper ar -f -c http://download.opensuse.org/tumbleweed/repo/oss/ repo-oss
 zypper ar -f -c http://download.opensuse.org/tumbleweed/repo/non-oss/ repo-non-oss
-zypper ar -f -c http://download.opensuse.org/tumbleweed/repo/debug/repo-debug
+zypper ar -f -c http://download.opensuse.org/tumbleweed/repo/debug/ repo-debug
 zypper ar -f -c http://download.opensuse.org/update/tumbleweed/ repo-update
-zypper ar -f -d -c http://download.opensuse.org/tumbleweed/repo/src-oss repo-src-oss
-zypper ar -f -d -c http://download.opensuse.org/tumbleweed/repo/src-non-oss repo-src-non-oss
+zypper ar -f -d -c http://download.opensuse.org/tumbleweed/repo/src-oss/ repo-src-oss
+zypper ar -f -d -c http://download.opensuse.org/tumbleweed/repo/src-non-oss/ repo-src-non-oss
 zypper ar -f -c http://ftp.gwdg.de/pub/linux/misc/packman/suse/openSUSE_Tumbleweed/ packman
 zypper ar -f -c http://download.opensuse.org/repositories/games/openSUSE_Tumbleweed/ games
 zypper ar -f -c https://download.opensuse.org/repositories/X11:/XOrg/openSUSE_Tumbleweed/ xorg_x11
@@ -15,5 +15,7 @@ zypper ar -f -c https://download.opensuse.org/repositories/Emulators/openSUSE_Tu
 zypper ref -non-interactive
 #upgrade zypper as this is importiant especialy on pre Leap based systems.  
 #Not doing this will result in an unstable system with no ability to fix the broken packages.
-#If this fails and sypper breaks, use the tumbleweed DVD to do an upgrade.
-zypper up zypper
+#If this fails and Zypper breaks, use the tumbleweed DVD to do an upgrade.
+zypper up zypper libzypp
+#an alternitive would be to wget zypper and libzypp and fore install them with rpm -i --nodeps.
+#doing so may lead to a broken zypper if the original distro is too old.
