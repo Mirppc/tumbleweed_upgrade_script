32a139306510edbf8a78c6f6b72bb1dd379dc7db
Mir_ppc <maninredd@yahoo.com>
2023-07-21 15:54:43 -0700

    Update install.sh
    remove the xorg repo as that causes conflicts and added the utility repo

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@ zypper ar -f -d -c http://download.opensuse.org/tumbleweed/repo/src-oss/ repo-sr
 zypper ar -f -d -c http://download.opensuse.org/tumbleweed/repo/src-non-oss/ repo-src-non-oss
 zypper ar -f -c http://ftp.gwdg.de/pub/linux/misc/packman/suse/openSUSE_Tumbleweed/ packman
 zypper ar -f -c http://download.opensuse.org/repositories/games/openSUSE_Tumbleweed/ games
-zypper ar -f -c https://download.opensuse.org/repositories/X11:/XOrg/openSUSE_Tumbleweed/ xorg_x11
+zypper ar -f -c https://download.opensuse.org/repositories/utilities/openSUSE_Factory/ utilities
 zypper ar -f -c https://download.opensuse.org/repositories/hardware/openSUSE_Tumbleweed/ hardware
 zypper ar -f -c https://download.opensuse.org/repositories/Emulators/openSUSE_Tumbleweed/ emulators
 #refresh repositories getting GPG keys and package lists
